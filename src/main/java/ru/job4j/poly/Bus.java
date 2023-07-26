853b51f8437a1d502c01f215768770925a82a04d
Pavel <pa11ady@yandex.ru>
2023-07-26 16:01:14 +0300

    4.2. Интерфейс Input [#181092 # [#181092 #432258]]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,21 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private double unitPrice = 60.0;
+    private int number;
+
     @Override
     public void go() {
-
+        System.out.println("Ехать!");
     }
 
     @Override
     public void passengers(int number) {
-
+        this.number = number;
     }
 
     @Override
     public double refuel(double amount) {
-        return 0;
+        return amount * unitPrice;
     }
 }
