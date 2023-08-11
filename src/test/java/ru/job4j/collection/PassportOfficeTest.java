c82c62973f56020dc8bdfa31e8cfeb6200772c5a
Pavel <pa11ady@yandex.ru>
2023-08-11 16:36:36 +0300

    1. Паспорт и жители [#212656 #435805]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -4,11 +4,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PassportOfficeTest {
+
     @Test
     public void whenTestAddMethod() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
+        Boolean rst = office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+        assertThat(rst).isTrue();
+    }
+
+    @Test
+    public void whenAddDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen duplicate = new Citizen("2f44a", "Duplicate");
+        PassportOffice office = new PassportOffice();
         office.add(citizen);
+        assertThat(office.add(duplicate)).isFalse();
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
 }
